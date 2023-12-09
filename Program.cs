82e6906bd8ff0c705765293a95bed95eaf74780f
Sergey Avtonomov <avtonomovsergei@yandex.ru>
2023-12-09 20:03:41 +0300

    Добавлено решение третьей задачи

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             Console.Clear();
             string stringLoad = "Введите номер задачи (от 1 до 3) => ";
             System.Console.Write(stringLoad);
-            Tasks(NumberInTerminal(3,stringLoad));
+            Tasks(NumberInTerminal(3,stringLoad,1));
         }
         static void Tasks(int num)
         {
@@ -43,10 +43,37 @@
                 {
                     System.Console.WriteLine("Задача 3: Задайте произвольный массив. Выведете его элементы,");
                     System.Console.WriteLine("начиная с конца. Использовать рекурсию, не использовать циклы.");
-
+                    int [] array = CreateArray(10,10,0);
+                    System.Console.WriteLine($"Произвольный массив => [{PrintArray(array)}]");
+                    System.Console.WriteLine($"Перевернутый массив => [{PrintArray(ArraySwap(array,array.Length-1))}]");
                     break;
                 }
             }
+        }
+        public static int [] CreateArray (int size, int max, int min){
+            int [] array  = new int[size];
+            Random rand = new();
+            for (int i = 0; i < size; i++)
+            {
+                array [i] = rand.Next(min,max+1);
+            }
+            return array;
+        }
+        public static string PrintArray(int [] array)
+        {
+            return string.Join(", ",array);
+        }
+        public static int [] ArraySwap(int [] array,int index)
+        {
+
+            if (index <= 0)
+            {
+                return array;
+            }
+            int temp = array[index];
+            array[index] = array[array.Length - index-1];
+            array[array.Length - index-1] = temp;
+            return ArraySwap(array,index -= 2);
         }
         public static int Ack(int m, int n)
         {
